4462df732472fcb4fee9ea348133a4e6542dfd18
hanke0 <hanke0@outlook.com>
2022-09-03 15:48:30 +0800

    Add a script for creating new default script

---
+++ new-script.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+usage="
+Usage: ${0##*/} filename
+Create a new standard bash script.
+"
+
+. "$(dirname "$(realpath "$0")")/base-for-all.sh"
+
+getopt_from_usage "$usage" "$@"
+
+for file in "${PARAMS[@]}"; do
+    cat >"$file" <<EOF
+#!/usr/bin/env bash
+
+usage="
+Usage: \${0##*/} [OPTION]...
+XXX
+
+OPTION:
+
+"
+
+. "$(dirname "$(realpath "\$0")")/base-for-all.sh"
+
+getopt_from_usage "\$usage" "\$@"
+
+EOF
+    chmod +x "$file"
+done
