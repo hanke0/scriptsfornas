21c1ccee3757dcb1e2acb401d8016971f715a107
hanke0 <hanke0@outlook.com>
2022-09-12 21:49:02 +0800

    Add conver-to-unix-text.sh

---
+++ convert-to-unix-text.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -e
+
+usage="
+Usage: ${0##*/} [OPTION]... [FILE]...
+Convert file encoding into UTF-8 and changes CRLF to LF.
+
+OPTION:
+    -s, --suffix=SUFFIX      suffix of output file, set empty to replace file.(default to replace original file)
+"
+
+. "/home/kehan/codes/scriptsfornas/base-for-all.sh"
+
+getopt_from_usage "$usage" "$@"
+
+if ! option_has_set "SUFFIX"; then
+    SUFFIX="utf8"
+fi
+
+chardet() {
+    if ! uchardet "$1"; then
+        exit 1
+    fi
+}
+
+for f in "${PARAMS[@]}"; do
+    if [ ! -f "$f" ]; then
+        continue
+    fi
+    chardet="$(chardet "$f")"
+    if [ "$chardet" = "UTF-8" ]; then
+        continue
+    fi
+    ext="$(filename_ext "$f")"
+    base="$(filename_base "$f")$SUFFIX"
+    if [ -z "$ext" ]; then
+        name="$(dirname "$f")/$base"
+    else
+        name="$(dirname "$f")/$base.$ext"
+    fi
+    tmp="$name.$RANDOM"
+    if ! iconv -f "$chardet" -t "UTF-8" -o "$tmp" "$f"; then
+        rm "$tmp"
+        echo >&2 "convert fail: $f"
+        exit 1
+    fi
+    if ! dos2unix "$tmp"; then
+        rm "$tmp"
+        echo >&2 "convert fail: $f"
+        exit 1
+    fi
+    mv "$tmp" "$name"
+done
