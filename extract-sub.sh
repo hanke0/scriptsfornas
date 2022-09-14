4018e8d91e97d5d7e16652aae591a22686f42607
hanke0 <hanke0@outlook.com>
2022-09-14 23:12:49 +0800

    Extract sub support 7z

--- extract-sub.sh
+++ extract-sub.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
-
 usage="
 Usage: ${0##*/} [OPTION]...
 Deeply extract compressed files, that contains sub titles, into one folder.
@@ -22,6 +19,10 @@ fi
 
 mkdir -p "$DESTINATION"
 DESTINATION="$(realpath "$DESTINATION")"
+if [ -n "$(ls "$DESTINATION")" ]; then
+    echo >&2 "output folder contain files, try rm -rf $DESTINATION"
+    exit 1
+fi
 
 extract() {
     case "$1" in
@@ -35,8 +36,10 @@ extract() {
     *.tar.*)
         tar -xf --directory "$DESTINATION" "$1"
         ;;
+    *.7z)
+        7z e -y -bb3 "-o$DESTINATION" "$1"
+        ;;
     *) ;;
-
     esac
 }
 FILES=()
@@ -51,7 +54,14 @@ for f in "${FILES[@]}"; do
     extract "$f"
 done
 
-find "$DESTINATION" \( -name "*.rar" -or -name "*.zip" -or -name "*.tar.*" \) -print0 |
+find "$DESTINATION" \( -name "*.rar" -or -name "*.zip" -or -name "*.tar.*" -or -name "*.7z" \) -print0 |
     while IFS= read -r -d '' tar; do
         extract "$tar"
+        rm "$tar"
+    done
+
+find "$DESTINATION" -type d -print0 |
+    while IFS= read -r -d '' folder; do
+        mv -f "$folder/"* "$DESTINATION/"
+        rmdir "$folder"
     done
