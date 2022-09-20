c35e67940843b5a5da1b8b137486d7cc9fa1628a
hanke0 <hanke0@outlook.com>
2022-09-21 01:30:40 +0800

    Add truncate large log files

---
+++ truncate-large-log.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+usage="
+Usage: ${0##*/} [OPTION]... [FILES]...
+Trucate larger log into max size.
+
+OPTION:
+    -m --max=SIZE        set default max size(default to 64M)
+"
+
+. "/home/kehan/codes/scriptsfornas/base-for-all.sh"
+
+getopt_from_usage "$usage" "$@"
+require_basic_commands
+require_command truncate
+
+if [ -z "$MAX" ]; then
+    MAX=64M
+fi
+
+for f in "${PARAMS[@]}"; do
+    if [ -d "$f" ]; then
+        find "$f" -type f -print0 | du -t "$MAX" --files0-from=- | awk '{print $2}' | xargs truncate -s 0 || true
+        continue
+    fi
+    if [ -f "$f" ]; then
+        du -t "$MAX" "$f" | awk '{print $2}' | xargs truncate -s 0 || true
+        continue
+    fi
+done
