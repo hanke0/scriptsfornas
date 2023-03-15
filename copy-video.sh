dcaf8cacb0a4a11c64062fd2a9e9499eeabbd93d
hanke <hanke0@outlook.com>
2023-03-15 10:06:04 +0000

    Add copy-video.sh

---
+++ copy-video.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+usage="
+Usage: ${0##*/} [OPTION]... <directory-contains-video> <target-directory>
+Copy all videos to one directory.
+
+OPTION:
+
+"
+
+# shellcheck source=/dev/null
+. "$(dirname "$(realpath "$0")")/base-for-all.sh"
+
+getopt_from_usage "$usage" "$@"
+require_basic_commands
+# require_command
