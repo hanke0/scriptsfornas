a856506d9393a4373c91d7e96897c4bf2ac1c1ac
hanke0 <hanke0@outlook.com>
2023-03-25 13:08:45 +0800

    Fix find type and mindepth

--- rename-video-folder-by-nfo.sh
+++ rename-video-folder-by-nfo.sh
@@ -24,7 +24,7 @@ doone() {
     local file title originaltitle year dest
     while IFS= read -r -d '' -u 5 file; do
         :
-    done 5< <(find "$1" -type f -maxdepth 1 -mindepth 1 -name '*.nfo' -print0)
+    done 5< <(find "$1" -maxdepth 1 -mindepth 1 -type f -name '*.nfo' -print0)
 
     if [ -z "$file" ]; then
         echo >&2 "can not find nfo file: $1"
