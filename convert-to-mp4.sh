fd15003d337eea408cf77657f7f17e25c629315a
hanke <hanke0@outlook.com>
2025-12-06 13:34:37 +0800

    add convert-to-mp4.sh

---
+++ convert-to-mp4.sh
@@ -0,0 +1,180 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+usage="
+Usage: ${0##*/} [OPTION]... input output
+Convert to mp4.
+
+OPTION:
+    -y, --yes     yes to all questions, overwrite output file without asking.
+    -c, --codec   codec to use, default to h264.
+	-a, --audio   audio codec to use, default to aac.
+"
+
+# shellcheck source=/dev/null
+. "$(dirname "$(realpath "$0")")/base-for-all.sh"
+
+getopt_from_usage "$usage" "$@"
+require_basic_commands
+# require_command
+
+FFMPEG="${FFMPEG:-ffmpeg}"
+
+input="${PARAMS[0]}"
+output="${PARAMS[1]}"
+if ! [ -f "$input" ]; then
+	echo >&2 "Not a file: $input"
+	exit 1
+fi
+
+if [ -z "$output" ]; then
+	echo >&2 "output not set, try --help for more information"
+	exit 1
+fi
+
+if [ -z "$CODEC" ]; then
+	CODEC="h264"
+fi
+
+if [ -z "$AUDIO_CODEC" ]; then
+	AUDIO_CODEC="aac"
+fi
+
+case "$CODEC" in
+h264)
+	CODEC="h264"
+	;;
+hevc)
+	CODEC="hevc"
+	;;
+*)
+	echo >&2 "unknown codec: $CODEC, select h264 or hevc"
+	exit 1
+	;;
+esac
+
+YESOPTION=
+if istrue "$YES"; then
+	YESOPTION='-y'
+fi
+
+case "$AUDIO_CODEC" in
+aac)
+	AUDIO_CODEC="aac"
+	;;
+*)
+	echo >&2 "unknown audio codec: $AUDIO_CODEC, aac is supported"
+	exit 1
+	;;
+esac
+
+audio_codec() {
+	ffprobe -v error -select_streams a:0 \
+		-show_entries \
+		stream=codec_name -of \
+		default=noprint_wrappers=1:nokey=1 \
+		"$1"
+}
+
+video_codec() {
+	ffprobe -v error -select_streams v:0 \
+		-show_entries \
+		stream=codec_name -of \
+		default=noprint_wrappers=1:nokey=1 \
+		"$1"
+}
+
+just_copy() {
+	local args
+	args=(
+		"$FFMPEG" -hide_banner $YESOPTION
+		-i "$1" -map 0
+		# video
+		-c:v copy
+		# audio
+		-c:a copy
+		# subtitles
+		-c:s copy
+		# move moov to the beginning of the file to improve online viewing experience
+		-movflags +faststart
+		"$2"
+	)
+	"${args[@]}"
+}
+
+copy_video_encode() {
+	local args
+	args=(
+		"$FFMPEG" -hide_banner $YESOPTION
+		-i "$1" -map 0
+		# video
+		-c:v copy
+		# audio
+		-c:a aac -b:a 128k
+		# subtitles
+		-c:s copy
+		# move moov to the beginning of the file to improve online viewing experience
+		-movflags +faststart
+		"$2"
+	)
+	"${args[@]}"
+}
+
+qsv_encode() {
+	local args
+	args=(
+		"$FFMPEG" -hide_banner $YESOPTION
+		-hwaccel qsv -hwaccel_output_format qsv
+		-i "$1" -map 0
+		# video
+		-c:v ${CODEC}_qsv -global_quality 21
+		# audio
+		-c:a aac -b:a 128k
+		# subtitles
+		-c:s copy
+		# move moov to the beginning of the file to improve online viewing experience
+		-movflags +faststart
+		"$2"
+	)
+	"${args[@]}"
+}
+
+cude_encode() {
+	local args
+	args=(
+		"$FFMPEG" -hide_banner $YESOPTION
+		-hwaccel cuda
+		-i "$1" -map 0
+		-c:v ${CODEC}_nvenc
+		# audio
+		-c:a aac -b:a 128k
+		# 字幕
+		-c:s copy
+		# 将视频文件的元信息（moov 块）移动到文件开头。这样，浏览器无需加载完整视频就能开始播放和跳转，极大提升在线观看体验
+		-movflags +faststart
+		"$2"
+	)
+	"${args[@]}"
+}
+
+support_hwaccel() {
+	"$FFMPEG" -hide_banner -hwaccels 2>&1 | grep -q "$1" >/dev/null 2>&1
+}
+
+case "$(video_codec "$input")_$(audio_codec "$input")" in
+$CODEC_$AUDIO_CODEC)
+	just_copy "$input" "$output"
+	;;
+$CODEC_*)
+	copy_video_encode "$input" "$output"
+	;;
+*)
+	if support_hwaccel "cuda"; then
+		cude_encode "$input" "$output"
+	else
+		qsv_encode "$input" "$output"
+	fi
+	;;
+esac
